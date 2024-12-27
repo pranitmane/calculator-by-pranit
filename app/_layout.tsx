39aa69dc385ca6c5bad81220879728e6ed682855
pranitmane <manepranit10@gmail.com>
2024-12-27 15:35:24 +0530

    nativewind setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView,View } from "react-native";
+import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{
+        headerShown:false
+      }}/>
+      {/* <StatusBar style="auto" /> */}
+    </Stack>
+  )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex justify-center items-center">
+      <Text className="text-xl text-orange-700">Hello world</Text>
     </View>
   );
 }
