1d3ec2b488c96562dfe89931b44467ca73749a21
pranitmane <manepranit10@gmail.com>
2025-01-07 22:31:17 +0530

    add light and dark mode

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,15 @@
 import { Link } from 'expo-router';
 import React, { useState, useEffect } from 'react';
-import { Text, View, Pressable, SafeAreaView } from 'react-native';
+import { Text, View, Pressable, SafeAreaView, Image } from 'react-native';
 import { twMerge } from 'tailwind-merge';
 import { evaluateExpression, formatExpression, inputValidation } from '../utils/calculate';
+import { useColorScheme } from "nativewind";
 
 export default function Index() {
   const [expression, setExpression] = useState<string>('0');
   const [result, setResult] = useState<string>('0');
   const [calculate, setCalculate] = useState<boolean>(false);
+  const { colorScheme, toggleColorScheme } = useColorScheme();
 
   useEffect(() => {
     setResult(() => {
@@ -57,24 +59,29 @@ export default function Index() {
     <Pressable
       onPress={() => handlePress(value)}
       className={twMerge(
-        'bg-gray-900 rounded-2xl p-4 flex-1 justify-center items-center active:scale-95 active:bg-gray-900/70 transition-all',
+        'bg-numbers rounded-xl border dark:border-white/15 border-black/20 p-4 flex-1 justify-center items-center active:scale-95 active:bg-numbersActive transition-all',
         buttonStyle
       )}
     >
-      <Text className={twMerge('text-4xl text-gray-200/70', textStyle)}>
+      <Text className={twMerge('text-4xl text-textNumbers', textStyle)}>
         {value}
       </Text>
     </Pressable>
   );
 
   return (
-    <SafeAreaView className={twMerge('flex-1 bg-gray-950 pt-safe')}>
+    <SafeAreaView className={twMerge('flex-1 p-4 gap-4 bg-primary pt-safe', colorScheme === 'dark' ? 'darkTheme' : 'lightTheme')}>
+      <View className='flex-row'>
+        <Pressable className='p-4 ml-auto border dark:border-white/15 border-black/20 rounded-xl active:scale-95' onPress={toggleColorScheme}>
+          <Text className='text-textNumbers'>{colorScheme}</Text>
+        </Pressable>
+      </View>
       {/* Display Section */}
-      <View className="flex-1 gap-2 p-4 justify-end items-end">
+      <View className="flex-1 gap-2 justify-end items-end">
         <Text
           className={twMerge(
             "transition-all duration-300 ease-in-out",
-            !calculate ? "text-7xl text-gray-200/70" : "text-5xl text-gray-400/70"
+            !calculate ? "text-7xl text-inputActive" : "text-5xl text-textNumbers"
           )}
         >
           {formatExpression(expression)}
@@ -83,8 +90,8 @@ export default function Index() {
           <Text
             className={twMerge(
               "transition-all duration-300 ease-in-out",
-              calculate ? "text-7xl text-gray-200/70" : "text-5xl text-gray-400/70",
-              result === 'Error' ? "text-red-500/70" : ""
+              calculate ? "text-7xl text-inputActive" : "text-5xl text-textNumbers",
+              result === 'Error' ? "text-clear" : ""
             )}
           >
             {"= " + formatExpression(result)}
@@ -92,51 +99,52 @@ export default function Index() {
         )}
       </View>
 
+      <View className="border-b dark:border-white/15 border-black/20" />
 
       {/* Keypad Section */}
-      <View className="flex-3 border-t border-gray-800 gap-2 p-4 max-h-[70%]">
+      <View className="flex-3 gap-1 max-h-[70%]">
         {/* Row 1 */}
-        <View className="flex-row gap-2">
-          {renderButton('AC', 'bg-red-800 active:bg-red-800/70')}
-          {renderButton('DEL', 'bg-gray-800 active:bg-gray-800/70')}
-          {renderButton('%', 'bg-gray-800 active:bg-gray-800/70')}
-          {renderButton('÷', 'bg-gray-800 active:bg-gray-800/70')}
+        <View className="flex-row gap-1">
+          {renderButton('AC', 'bg-clear active:bg-clearActive', 'text-textClear text-3xl')}
+          {renderButton('DEL', 'bg-operators active:bg-operatorsActive', 'text-textOperators text-3xl')}
+          {renderButton('%', 'bg-operators active:bg-operatorsActive', 'text-textOperators')}
+          {renderButton('÷', 'bg-operators active:bg-operatorsActive', 'text-textOperators')}
         </View>
         {/* Row 2 */}
-        <View className="flex-row gap-2">
+        <View className="flex-row gap-1">
           {renderButton('7')}
           {renderButton('8')}
           {renderButton('9')}
-          {renderButton('×', 'bg-gray-800 active:bg-gray-800/70')}
+          {renderButton('×', 'bg-operators active:bg-operatorsActive', 'text-textOperators')}
         </View>
         {/* Row 3 */}
-        <View className="flex-row gap-2">
+        <View className="flex-row gap-1">
           {renderButton('4')}
           {renderButton('5')}
           {renderButton('6')}
-          {renderButton('-', 'bg-gray-800 active:bg-gray-800/70')}
+          {renderButton('-', 'bg-operators active:bg-operatorsActive', 'text-textOperators')}
         </View>
         {/* Row 4 */}
-        <View className="flex-row gap-2">
+        <View className="flex-row gap-1">
           {renderButton('1')}
           {renderButton('2')}
           {renderButton('3')}
-          {renderButton('+', 'bg-gray-800 active:bg-gray-800/70')}
+          {renderButton('+', 'bg-operators active:bg-operatorsActive', 'text-textOperators')}
         </View>
         {/* Row 5 */}
-        <View className="flex-row gap-2 min-h-fit">
+        <View className="flex-row gap-1 min-h-fit">
           <View className='flex-1'>
-            {renderButton('0')}
+            {renderButton('0', 'rounded-bl-3xl')}
           </View>
-          <View className='flex-row gap-2 flex-1 relative'>
+          <View className='flex-row gap-1 flex-1 relative'>
             {renderButton('.')}
-            {renderButton('=', 'bg-green-800 active:bg-green-800/70')}
+            {renderButton('=', 'bg-equalOperator active:bg-equalOperatorActive rounded-br-3xl', 'text-textEqualOperator')}
           </View>
         </View>
-        <Text className='text-center text-gray-500 text-lg font-light'>
-          Calculator by <Link className=' underline text-sky-500' href='https://pranitmane.com'>Pranit</Link>
-        </Text>
       </View>
+      {/* <Text className='text-center text-green-100/50 text-lg font-light'>
+        Calculator by <Link className=' underline' href='https://pranitmane.com'>Pranit</Link>
+      </Text> */}
     </SafeAreaView>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./app/**/*.{js,jsx,ts,tsx}"];
-export const presets = [require("nativewind/preset")];
-export const theme = {
-  extend: {},
+module.exports = {
+  content: ["./src/app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  darkMode: "class",
+  theme:{
+    extend:{
+      colors:{
+        primary: 'var(--bg-primary)',
+        inputActive: 'var(--text-input-active)',
+        numbers: 'var(--bg-numbers)',
+        numbersActive: 'var(--bg-numbers-active)',
+        textNumbers: 'var(--text-numbers)',
+        operators: 'var(--bg-operators)',
+        operatorsActive: 'var(--bg-operators-active)',
+        textOperators: 'var(--text-operators)',
+        equalOperator: 'var(--bg-equal-operator)',
+        equalOperatorActive: 'var(--bg-equal-operator-active)',
+        textEqualOperator: 'var(--text-equal-operator)',
+        clear: 'var(--bg-clear)',
+        clearActive: 'var(--bg-clear-active)',
+        textClear: 'var(--text-clear)',
+      }
+    }
+  },
+  plugins: [],
 };
-export const plugins = [];
-
